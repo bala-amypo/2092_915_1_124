18e7434e47d24054572775648dc2a70a2d7cfc4b
bala-amypo <bala@amypo.in>
2025-12-23 07:36:42 +0000

    User commit

--- src/main/java/com/example/demo/security/CustomUserDetailsService.java
+++ src/main/java/com/example/demo/security/CustomUserDetailsService.java
@@ -1,36 +1,23 @@
 package com.example.demo.security;
 
 import com.example.demo.entity.UserAccount;
-import org.springframework.security.core.*;
-import org.springframework.security.core.userdetails.UserDetails;
+import com.example.demo.repository.UserAccountRepository;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
 
-import java.util.Collections;
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
 
-public class CustomUserDetails implements UserDetails {
+    private final UserAccountRepository repository;
 
-    private final UserAccount user;
-
-    public CustomUserDetails(UserAccount user) {
-        this.user = user;
+    public CustomUserDetailsService(UserAccountRepository repository) {
+        this.repository = repository;
     }
 
     @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return Collections.emptyList();
+    public UserDetails loadUserByUsername(String email) {
+        UserAccount user = repository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return new CustomUserDetails(user);
     }
-
-    @Override
-    public String getPassword() {
-        return user.getPassword();
-    }
-
-    @Override
-    public String getUsername() {
-        return user.getEmail();
-    }
-
-    @Override public boolean isAccountNonExpired() { return true; }
-    @Override public boolean isAccountNonLocked() { return true; }
-    @Override public boolean isCredentialsNonExpired() { return true; }
-    @Override public boolean isEnabled() { return true; }
 }
