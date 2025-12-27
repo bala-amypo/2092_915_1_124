ba798734245b831dc62d49a91cb4b36923b5bc5c
bala-amypo <bala@amypo.in>
2025-12-27 04:33:18 +0000

    User commit

--- src/main/java/com/example/demo/repository/DiversityTargetRepository.java
+++ src/main/java/com/example/demo/repository/DiversityTargetRepository.java
@@ -2,9 +2,10 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.DiversityTarget;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.List;
 
 public interface DiversityTargetRepository extends JpaRepository<DiversityTarget, Long> {
-    List<DiversityTarget> findByTargetYear(Integer year);
-    List<DiversityTarget> findByIsActiveTrue();
+
+    List<DiversityTarget> findByActiveTrue();
 }
